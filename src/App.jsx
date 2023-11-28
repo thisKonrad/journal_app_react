f46c6c6c3bbbd666850121abeb09768e58b68ad6
this.Konrad <konradlebt@gmail.com>
2023-11-28 16:27:26 +0100

    enhance styling basics

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
 import './App.css'
+import './colors.css'
+import Header from './src/components/header/header.jsx';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     
+    <Header/>
     </>
   )
 }
---
+++ src/components/header/header.jsx
@@ -0,0 +1,11 @@
+import './header.css'
+
+export default function Header(){
+
+    return <>
+            <header>
+                <h1>Journal</h1>
+            </header>
+        </>
+
+}
