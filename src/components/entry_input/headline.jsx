b488e113ce4857ef112082484ec5769203bbdbea
this.Konrad <konradlebt@gmail.com>
2023-11-29 09:24:18 +0100

    add headline to form

---
+++ src/components/entry_input/headline.jsx
@@ -0,0 +1,12 @@
+import './entry_form.css'
+
+export default function EntryFormHeadline(){
+
+    return( <>
+            <div>
+            <h2 className="new_entry">New Entry:</h2>
+            </div>
+        </>
+    )
+
+}
